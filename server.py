33cdde40a67ff3c4512e21c70caf940d6ab49035
Dezhi “Andy” Fang <andyfang.dz@gmail.com>
2017-04-13 14:08:50 -0400

    basic routes

--- server.py
+++ server.py
@@ -3,8 +3,22 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 if __name__ == "__main__":
     app.run()
